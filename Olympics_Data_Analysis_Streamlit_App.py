dfe8eb11f64a2d353a74a2b2896a770b4455bccb
Zaid Shaikh <117260335+skzaid091@users.noreply.github.com>
2024-05-02 07:17:30 +0530

    Update Olympics_Data_Analysis_Streamlit_App.py

--- Olympics_Data_Analysis_Streamlit_App.py
+++ Olympics_Data_Analysis_Streamlit_App.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 # import plotly.express as px
 # from matplotlib import pyplot as plt
-import seaborn as sns
+# import seaborn as sns
 import plotly.figure_factory as ff
 import preprocessor, help
 
